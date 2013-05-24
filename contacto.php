9ac11f32107922a521a65279e07cb5735b5dc9c0
Ezequiel Gonzalez <el.palme@gmail.com>
2013-05-24 01:09:35 -0300

    Comit gral.
    agrege bootstrap y sus cosas.
    modifiqe index y css..
    ademas cree el contactos el cual falta acomodar y validar el form.
    agrege la carpeta imagenes la cual hay q depurar.

---
+++ contacto.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> BP Group Seguros - Contacto</title>
+    <meta name="author" content="Porro Betina González Augusto">
+	<meta name="description" content="Sitio Web de BP Group Seguros - Productores de Seguros">
+    <meta name="keywords" content="seguros,granizo,siniestro,robo,choques,vientos,silo bolsa,suelo duro,autos,flota,bodega,maíz,girasol,soja,cebada,trigo,seguro,comercio,integral,consorcio,vivienda,combinado,country,countries,accidente,personal,asesoramiento,productor,embarcaciones,mercadería,tractor,maquinaria,rural,implemento rural,responsabilidad civil,transporte,aéreo,terrestre,maritimo,seguro técnico,hoteles,náutica,casa,campo,agropecuario,seguros para establecimientos agrícolas y ganaderos,accidentes personales,cosecha fina,cosecha gruesa" >
+    <meta name="Language" content="Spanish">
+	<meta content="text/html; charset=utf-8" http-equiv="content-type">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css" type="text/css">
+	<link rel="stylesheet" href="estilo.css" type="text/css">
+	<link rel="javascript" href="bootstrap/js/bootstrap.js" type="text/javascript" >
+</head>
+
+<body>
+
+<div class="top"></div>
+
+<div class="all">
+	<div id="cabezera">
+
+		<br /> <br />
+
+		<div id="logo">
+			<a href="index.php" target="_self"><img src="imagenes/bpgroup.jpg" alt="BP Group Seguros" border="0"></a>
+		</div>
+
+		<div id="menu">   
+			<ul>
+				<li><a href="index.php">Inicio</a></li>
+				<li><a href="historia.php">Quienes Somos?</a></li>
+				<li><a href="seguros.php">Seguros</a></li>
+				<li><a href="contacto.php">Contacto</a></li>
+				<li><a href="links.php">Links Utiles</a></li>
+			</ul>
+		</div>     
+		<!-- FIN MENU-->
+	</div>
+	<!-- FIN CABEZERA -->
+
+	<div class="page" style="height: 900px;">
+		<div class="mapa">
+			<address style="font-size: 13px;">
+				<strong>Oficina de Seguros</strong>
+				<br>
+				Av. 19 e/ 46 y 47
+				<br>
+				<abbr title="Telefono">Tel:</abbr> 221 482-0957
+				<br>
+				<abbr title="Celular">Cel:</abbr> 221 (15)6159582
+				<br>
+				La Plata, Buenos Aires, Argentina
+				<br>
+			</address>
+			<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=201554196545272614248.0004dd6d93530d0bbbf0f&amp;ie=UTF8&amp;t=m&amp;ll=-34.924469,-57.963781&amp;spn=0.010556,0.012875&amp;z=15&amp;output=embed"></iframe><br /><small>Ver <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=201554196545272614248.0004dd6d93530d0bbbf0f&amp;ie=UTF8&amp;t=m&amp;ll=-34.924469,-57.963781&amp;spn=0.010556,0.012875&amp;z=15&amp;source=embed" style="color:#0000FF;text-align:left">BP Group Seguros</a> en un mapa mas grande</small>				
+		</div>
+		<div class="formulario-contacto">
+			<form class="form-vertical">
+				<fieldset>
+					<legend> Contacto </legend>
+					<label>Nombre</label>
+					<input type="text" class="input-large" placeholder="Escriba su nombre.." required>
+					
+					<label>Apellido</label>
+					<input type="text" class="input-large" placeholder="Escriba su apellido.." required>
+					
+					<label>Email</label>
+					<input type="email" class="input-large" placeholder="ejemplo@servidor.com" required>
+					
+					<label>Telefono</label>
+					<input type="tel" class="input-large" placeholder="Escriba su telefono.." required>
+					<span class="help-block" style="font-size: 10px;margin-top: -12px;"> Sin Espacios. Ej: 2214517853 </span>
+					
+					<label>Consulta</label>
+					<textarea rows="5"></textarea>
+					
+					<div>
+						<button type="submit" class="btn btn-primary">Enviar</button>
+					</div>
+				</fieldset>
+			</form>
+		</div>
+	</div>
+	<!-- FIN DE PAGE -->
+
+	<div class="pie">
+		<br /> 2013 BP Group Seguros todos los derechos reservados.
+	</div>
+
+</div>                             <!-- FIN DE ALL -->
+
+<div class="bottom"></div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> BP Group Seguros </title>
+    <meta name="author" content="Porro Betina González Augusto">
+	<meta name="description" content="Sitio Web de BP Group Seguros - Productores de Seguros">
+    <meta name="keywords" content="seguros,granizo,siniestro,robo,choques,vientos,silo bolsa,suelo duro,autos,flota,bodega,maíz,girasol,soja,cebada,trigo,seguro,comercio,integral,consorcio,vivienda,combinado,country,countries,accidente,personal,asesoramiento,productor,embarcaciones,mercadería,tractor,maquinaria,rural,implemento rural,responsabilidad civil,transporte,aéreo,terrestre,maritimo,seguro técnico,hoteles,náutica,casa,campo,agropecuario,seguros para establecimientos agrícolas y ganaderos,accidentes personales,cosecha fina,cosecha gruesa" >
+    <meta name="Language" content="Spanish">
+	<meta content="text/html; charset=utf-8" http-equiv="content-type">
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+	<link rel="javascript" href="bootstrap/js/bootstrap.js" type="text/javascript" >
+</head>
+
+<body>
+
+<div class="top"></div>
+
+<div class="all">
+	<div id="cabezera">
+
+		<br /> <br />
+
+		<div id="logo">
+			<a href="index.php" target="_self"><img src="imagenes/bpgroup.jpg" alt="BP Group Seguros" border="0"></a>
+		</div>
+
+		<div id="menu">   
+			<ul>
+				<li><a href="index.php">Inicio</a></li>
+				<li><a href="historia.php">¿Quienes Somos?</a></li>
+				<li><a href="seguros.php">Seguros</a></li>
+				<li><a href="contacto.php">Contacto</a></li>
+				<li><a href="links.php">Links Utiles</a></li>
+			</ul>
+		</div>     
+		<!-- FIN MENU-->
+	</div>
+	<!-- FIN CABEZERA -->
+
+	<div class="page">
+		<div class="contenido">
+			<img src="imagenes/local3.jpg" alt="BP Group Seguros" border="0" width="950" height="650">
+			<br /><br />
+			<center><img src="imagenes/bannerfedpat.jpg" alt="Federacion Patronal Seguros S.A." border="0" style="margin-bottom: -3px;"></center>
+		</div>
+	</div>
+	<!-- FIN DE PAGE -->
+
+	<div class="pie">
+		<br /> ©2013 BP Group Seguros todos los derechos reservados.
+	</div>
+
+</div>                             <!-- FIN DE ALL -->
+
+<div class="bottom"></div>
+
+</body>
+</html>
